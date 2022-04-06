c8a4ce0152585d41442e494305d4ad8a4e50738b
kgw7401 <kgw7401@gmail.com>
2022-04-06 14:29:51 +0900

    Fix : PostgreSQL 컬럼 대소문자 구분 문제 해결

--- aladin_api.py
+++ aladin_api.py
@@ -9,26 +9,26 @@ def used_book_info(TTBKey):
 
     book_lists = []
 
-    for i in range(10):
+    for i in range(1, 11):
         url = f"http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey={TTBKey}&QueryType=ItemNewAll&SearchTarget=Used&SubSearchTarget=Book&MaxResults=50&start={i}&output=js&Version=20131101&OptResult=usedList"
         res = requests.get(url)
         items = json.loads(res.text)['item']
 
         for item in items:
             book_dict = {}
-            book_dict['itemId'] = item['itemId']
+            book_dict['itemid'] = item['itemId']
             book_dict['title'] = item['title'][5:]
             book_dict['author'] = item['author'].split(',')[0].split('(')[0]
-            book_dict['categoryName_large'] = item['categoryName'].split('>')[2] if len(item['categoryName'].split('>')) > 2 else "UNKNOWN"
-            book_dict['categoryName_small'] = item['categoryName'].split('>')[3] if len(item['categoryName'].split('>')) > 3 else "UNKNOWN"
-            book_dict['customerReviewRank'] = item['customerReviewRank']
-            book_dict['priceSales'] = item['priceSales']
-            book_dict['priceStandard'] = item['priceStandard']
-            book_dict['pubDate'] = item['pubDate']
+            book_dict['categoryname_large'] = item['categoryName'].split('>')[2] if len(item['categoryName'].split('>')) > 2 else "UNKNOWN"
+            book_dict['categoryname_small'] = item['categoryName'].split('>')[3] if len(item['categoryName'].split('>')) > 3 else "UNKNOWN"
+            book_dict['customer_review_rank'] = item['customerReviewRank']
+            book_dict['pricesales'] = item['priceSales']
+            book_dict['pricestandard'] = item['priceStandard']
+            book_dict['pubdate'] = item['pubDate']
             book_dict['publisher'] = item['publisher']
-            book_dict['aladinUsed_itemCount'] = int(item['subInfo']['usedList']['aladinUsed']['itemCount'])
-            book_dict['userUsed_itemCount'] = int(item['subInfo']['usedList']['userUsed']['itemCount'])
-            book_dict['userUsed_minPrice'] = item['subInfo']['usedList']['userUsed']['minPrice']
+            book_dict['aladinused_itemcount'] = int(item['subInfo']['usedList']['aladinUsed']['itemCount'])
+            book_dict['userused_itemcount'] = int(item['subInfo']['usedList']['userUsed']['itemCount'])
+            book_dict['userused_minprice'] = item['subInfo']['usedList']['userUsed']['minPrice']
 
             book_lists.append(book_dict)
         print(f"{i}번째 배치 완료!")
@@ -41,7 +41,7 @@ def insert_row(cursor, data):
     col = ', '.join(data)
     place_holders = ', '.join(['%s']*len(data))
     key_holders = ', '.join([k+'=%s' for k in data.keys()])
-    que = 'INSERT INTO used_book ({}) VALUES ({}) ON CONFLICT (itemId) DO UPDATE SET {}'.format(col, place_holders, key_holders)
+    que = 'INSERT INTO used_book ({}) VALUES ({}) ON CONFLICT (itemid) DO UPDATE SET {}'.format(col, place_holders, key_holders)
     cursor.execute(que, list(data.values())*2)
 
 def main():
